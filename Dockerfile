f192e1efd9ce92b6ed4d5ee138d45194aab330fb
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-02-06 14:11:46 +0000

    integrate tranmission

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM debian
 
+ENV TRANSMISSION_DOWNLOADS_PATH="/media/downloads"
+ENV SERVARR_APP = "/srv"
+ENV LOGS_PATH = "/var/log"
+
 # Disable Prompt During Packages Installation
 ARG DEBIAN_FRONTEND=noninteractive
 
@@ -14,7 +18,8 @@ RUN apt update && apt-get install  --no-install-recommends -y \
       nginx-extras \
       supervisor \ 
       procps \
-      ca-certificates
+      ca-certificates \
+      transmission-daemon
 
 RUN rm -rf /var/lib/apt/lists/*
 RUN apt clean
@@ -24,15 +29,22 @@ COPY install.sh /install.sh
 RUN chmod +x /install.sh
 RUN bash /install.sh
 
+RUN mkdir -p ${TRANSMISSION_DOWNLOADS_PATH}
+RUN mkdir -p ${TRANSMISSION_DOWNLOADS_PATH}/completed
+RUN mkdir -p ${TRANSMISSION_DOWNLOADS_PATH}/incompleted
+
 RUN chown -R www-data:www-data /var/www/html
 
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+COPY assets/** ${SERVARR_APP}/Homer/assets
 
-VOLUME [ "~/.config/", "/opt", "/etc/nginx", "/usr/share/nginx/html" ]
+COPY transmission/** /etc/transmission-daemon/
+
+VOLUME [ "~/.config/", ${SERVARR_APP}, "/etc/nginx", "/usr/share/nginx/html" ]
 
 # Expose Port for the Application 
 EXPOSE 80/tcp
-
+EXPOSE 51413/tcp
 # Run the Nginx server
 CMD ["/usr/bin/supervisord"]
--- install.sh
+++ install.sh
@@ -5,21 +5,27 @@ if [ "$EUID" -ne 0 ]; then
       exit
 fi
 
+function Homer() {
+      wget --no-check-certificate "https://github.com/bastienwirtz/homer/releases/latest/download/homer.zip" -O Homer.zip
+      unzip Homer.zip -d $SERVARR_APP/Homer
+      rm Homer.zip
+}
+
 function Readar() {
       echo "Download Readarr"
       wget --no-check-certificate --content-disposition 'http://readarr.servarr.com/v1/update/develop/updatefile?os=linux&runtime=netcore&arch=x64'
       echo "Extract Readarr"
       tar -xvzf Readarr*.linux*.tar.gz
-      echo "Move Readarr to /opt/"
-      mv Readarr /opt/
-      echo "Autorisation readarr in /opt/Readarr"
-      chown readarr:readarr -R /opt/Readarr
+      echo "Move Readarr to $SERVARR_APP/"
+      mv Readarr $SERVARR_APP/
+      echo "Autorisation readarr in $SERVARR_APP/Readarr"
+      chown readarr:readarr -R $SERVARR_APP/Readarr
       echo "Remove Readarr*.linux*.tar.gz"
       rm Readarr*.linux*.tar.gz
-      /opt/Readarr/Readarr -nobrowser &
+      $SERVARR_APP/Readarr/Readarr -nobrowser &
       sleep 10s
       sed -i 's|<UrlBase></UrlBase>|<UrlBase>/readarr</UrlBase>|g' ~/.config/Readar/config.xml
-      pkill -f /opt/Readarr
+      pkill -f $SERVARR_APP/Readarr
 }
 
 function Radarr() {
@@ -27,16 +33,16 @@ function Radarr() {
       wget --no-check-certificate --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
       echo "Extract Radarr"
       tar -xvzf Radarr*.linux*.tar.gz
-      echo "Move Radarr /opt/"
-      mv Radarr /opt/
-      echo "Autorisation Radarr in /opt/Radarr"
-      chown radarr:radarr -R /opt/Radarr
+      echo "Move Radarr $SERVARR_APP/"
+      mv Radarr $SERVARR_APP/
+      echo "Autorisation Radarr in $SERVARR_APP/Radarr"
+      chown radarr:radarr -R $SERVARR_APP/Radarr
       echo "Remove Radarr*.linux*.tar.gz"
       rm Radarr*.linux*.tar.gz
-      /opt/Radarr/Radarr -nobrowser &
+      $SERVARR_APP/Radarr/Radarr -nobrowser &
       sleep 10s
       sed -i 's|<UrlBase></UrlBase>|<UrlBase>/radarr</UrlBase>|g' ~/.config/Radarr/config.xml
-      pkill -f /opt/Radarr
+      pkill -f $SERVARR_APP/Radarr
       wait
 }
 
@@ -45,16 +51,16 @@ function Sonarr() {
       wget --no-check-certificate --content-disposition 'http://services.sonarr.tv/v1/download/master/latest?version=4&os=linux&runtime=netcore&arch=x64'
       echo "Extract Sonarr"
       tar -xvzf Sonarr*.linux*.tar.gz
-      echo "Move Sonarr /opt/"
-      mv Sonarr/ /opt
-      echo "Autorisation Sonarr in /opt/Sonarr"
-      chown -R root:media /opt/Sonarr
+      echo "Move Sonarr $SERVARR_APP/"
+      mv Sonarr/ $SERVARR_APP
+      echo "Autorisation Sonarr in $SERVARR_APP/Sonarr"
+      chown -R root:media $SERVARR_APP/Sonarr
       echo "Remove Sonarr*.linux*.tar.gz"
       rm Sonarr*.linux*.tar.gz
-      /opt/Sonarr/Sonarr -nobrowser &
+      $SERVARR_APP/Sonarr/Sonarr -nobrowser &
       sleep 10s
       sed -i 's|<UrlBase></UrlBase>|<UrlBase>/sonarr</UrlBase>|g' ~/.config/Sonarr/config.xml
-      pkill -f /opt/Sonarr
+      pkill -f $SERVARR_APP/Sonarr
 }
 
 function Lidarr() {
@@ -62,16 +68,16 @@ function Lidarr() {
       wget --no-check-certificate --content-disposition 'http://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
       echo "Extract Lidarr"
       tar -xvzf Lidarr*.linux*.tar.gz
-      echo "Move Lidarr /opt/"
-      mv Lidarr/ /opt
-      echo "Autorisation Lidarr in /opt/Lidarr"
-      chown -R root:media /opt/Lidarr
+      echo "Move Lidarr $SERVARR_APP/"
+      mv Lidarr/ $SERVARR_APP
+      echo "Autorisation Lidarr in $SERVARR_APP/Lidarr"
+      chown -R root:media $SERVARR_APP/Lidarr
       echo "Remove Lidarr*.linux*.tar.gz"
       rm Lidarr*.linux*.tar.gz
-      /opt/Lidarr/Lidarr -nobrowser &
+      $SERVARR_APP/Lidarr/Lidarr -nobrowser &
       sleep 10s
       sed -i 's|<UrlBase></UrlBase>|<UrlBase>/lidarr</UrlBase>|g' ~/.config/Lidarr/config.xml
-      pkill -f /opt/Lidarr
+      pkill -f $SERVARR_APP/Lidarr
 }
 
 function Prowlarr() {
@@ -79,16 +85,16 @@ function Prowlarr() {
       wget --no-check-certificate --content-disposition 'http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
       echo "Extract Prowlarr"
       tar -xvzf Prowlarr*.linux*.tar.gz
-      echo "Move Prowlarr /opt/"
-      mv Prowlarr/ /opt
-      echo "Autorisation Prowlarr in /opt/Prowlarr"
-      chown prowlarr:prowlarr -R /opt/Prowlarr
+      echo "Move Prowlarr $SERVARR_APP/"
+      mv Prowlarr/ $SERVARR_APP
+      echo "Autorisation Prowlarr in $SERVARR_APP/Prowlarr"
+      chown prowlarr:prowlarr -R $SERVARR_APP/Prowlarr
       echo "Remove Prowlarr*.linux*.tar.gz"
       rm Prowlarr*.linux*.tar.gz
-      /opt/Prowlarr/Prowlarr -nobrowser &
+      $SERVARR_APP/Prowlarr/Prowlarr -nobrowser &
       sleep 10s
       sed -i 's|<UrlBase></UrlBase>|<UrlBase>/prowlarr</UrlBase>|g' ~/.config/Prowlarr/config.xml
-      pkill -f /opt/Prowlarr
+      pkill -f $SERVARR_APP/Prowlarr
 
 }
 
