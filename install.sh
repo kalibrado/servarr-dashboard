ce2045a33b938f95bdd7e0242922736fe9f26e89
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-02-07 08:36:56 +0000

    bugfix: path home

--- install.sh
+++ install.sh
@@ -4,11 +4,10 @@ if [ "$EUID" -ne 0 ]; then
     echo "Please run as root."
     exit
 fi
-echo "Got to Home"
-cd ~
+
 function Homer() {
-     echo "Download Homer "
-    wget -q --no-check-certificate "https://github.com/bastienwirtz/homer/releases/latest/download/homer.zip" -O Homer.zip
+     echo "Download Homer ..."
+    wget -q --show-progress --no-check-certificate "https://github.com/bastienwirtz/homer/releases/latest/download/homer.zip" -O Homer.zip
     echo "Unzip Homer.zip in $SERVARR_APP/Homer"
     unzip -qq Homer.zip -d $SERVARR_APP/Homer
     echo "Delete homer.zip"
@@ -16,8 +15,8 @@ function Homer() {
 }
 
 function Readar() {
-    echo "Download Readarr"
-    wget -q --no-check-certificate --content-disposition 'http://readarr.servarr.com/v1/update/develop/updatefile?os=linux&runtime=netcore&arch=x64'
+    echo "Download Readarr..."
+    wget -q --show-progress --no-check-certificate --content-disposition 'http://readarr.servarr.com/v1/update/develop/updatefile?os=linux&runtime=netcore&arch=x64'
     echo "Extract Readarr"
     tar -xvzf Readarr*.linux*.tar.gz >/dev/null 2>&1
     echo "Move Readarr to $SERVARR_APP/"
@@ -27,15 +26,15 @@ function Readar() {
     echo "Remove Readarr*.linux*.tar.gz"
     rm Readarr*.linux*.tar.gz
     $SERVARR_APP/Readarr/Readarr -nobrowser >/dev/null 2>&1 &
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/readarr</UrlBase>|g' /.config/Readarr/config.xml
+    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/readarr</UrlBase>|g' ~/.config/Readarr/config.xml
     sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g'  /.config/Readarr/config.xml
     sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g'  /.config/Readarr/config.xml
     pkill -f $SERVARR_APP/Readarr
 }
 
 function Radarr() {
-    echo "Download Radarr"
-    wget -q --no-check-certificate --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+    echo "Download Radarr..."
+    wget -q --show-progress --no-check-certificate --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
     echo "Extract Radarr"
     tar -xvzf Radarr*.linux*.tar.gz >/dev/null 2>&1
     echo "Move Radarr $SERVARR_APP/"
@@ -45,7 +44,7 @@ function Radarr() {
     echo "Remove Radarr*.linux*.tar.gz"
     rm Radarr*.linux*.tar.gz
     $SERVARR_APP/Radarr/Radarr -nobrowser >/dev/null 2>&1 &
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/radarr</UrlBase>|g' /.config/Radarr/config.xml
+    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/radarr</UrlBase>|g' ~/.config/Radarr/config.xml
     sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g'  /.config/Radarr/config.xml
     sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g'  /.config/Radarr/config.xml
     pkill -f $SERVARR_APP/Radarr
@@ -53,8 +52,8 @@ function Radarr() {
 }
 
 function Sonarr() {
-    echo "Download Sonarr"
-    wget -q --no-check-certificate --content-disposition 'http://services.sonarr.tv/v1/download/master/latest?version=4&os=linux&runtime=netcore&arch=x64'
+    echo "Download Sonarr..."
+    wget -q --show-progress --no-check-certificate --content-disposition 'http://services.sonarr.tv/v1/download/master/latest?version=4&os=linux&runtime=netcore&arch=x64'
     echo "Extract Sonarr"
     tar -xvzf Sonarr*.linux*.tar.gz >/dev/null 2>&1
     echo "Move Sonarr $SERVARR_APP/"
@@ -64,15 +63,15 @@ function Sonarr() {
     echo "Remove Sonarr*.linux*.tar.gz"
     rm Sonarr*.linux*.tar.gz
     $SERVARR_APP/Sonarr/Sonarr -nobrowser >/dev/null 2>&1 &
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/sonarr</UrlBase>|g' /.config/Sonarr/config.xml
+    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/sonarr</UrlBase>|g' ~/.config/Sonarr/config.xml
     sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g'  /.config/Sonarr/config.xml
     sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g'  /.config/Sonarr/config.xml
     pkill -f $SERVARR_APP/Sonarr
 }
 
 function Lidarr() {
-    echo "Download Lidarr"
-    wget -q --no-check-certificate --content-disposition 'http://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+    echo "Download Lidarr..."
+    wget -q --show-progress --no-check-certificate --content-disposition 'http://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
     echo "Extract Lidarr"
     tar -xvzf Lidarr*.linux*.tar.gz >/dev/null 2>&1
     echo "Move Lidarr $SERVARR_APP/"
@@ -82,15 +81,15 @@ function Lidarr() {
     echo "Remove Lidarr*.linux*.tar.gz"
     rm Lidarr*.linux*.tar.gz
     $SERVARR_APP/Lidarr/Lidarr -nobrowser >/dev/null 2>&1 &
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/lidarr</UrlBase>|g' /.config/Lidarr/config.xml
+    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/lidarr</UrlBase>|g' ~/.config/Lidarr/config.xml
     sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g'  /.config/Lidarr/config.xml
     sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g'  /.config/Lidarr/config.xml
     pkill -f $SERVARR_APP/Lidarr
 }
 
 function Prowlarr() {
-    echo "Download Prowlarr"
-    wget -q --no-check-certificate --content-disposition 'http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
+    echo "Download Prowlarr..."
+    wget -q --show-progress --no-check-certificate --content-disposition 'http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
     echo "Extract Prowlarr"
     tar -xvzf Prowlarr*.linux*.tar.gz >/dev/null 2>&1
     echo "Move Prowlarr $SERVARR_APP/"
@@ -100,7 +99,7 @@ function Prowlarr() {
     echo "Remove Prowlarr*.linux*.tar.gz"
     rm Prowlarr*.linux*.tar.gz
     $SERVARR_APP/Prowlarr/Prowlarr -nobrowser >/dev/null 2>&1 &
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/prowlarr</UrlBase>|g' /.config/Prowlarr/config.xml
+    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/prowlarr</UrlBase>|g' ~/.config/Prowlarr/config.xml
     sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g'  /.config/Prowlarr/config.xml
     sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g'  /.config/Prowlarr/config.xml
     pkill -f $SERVARR_APP/Prowlarr
