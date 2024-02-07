e48d0130e7f9a76b26bf56f4ede3211859bf246a
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-02-07 13:32:46 +0000

    refacto

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,30 @@
 FROM debian
 
-MAINTAINER kalibrado
-
-ARG DEBIAN_FRONTEND=noninteractive
 ENV TRANSMISSION_DOWNLOADS_PATH="/media/downloads"
 ENV SERVARR_APP="/srv"
 ENV LOGS_PATH="/var/log"
 ENV SERVARR_THEME="dark"
 
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN mkdir -p "$TRANSMISSION_DOWNLOADS_PATH/completed"
+RUN mkdir -p "$TRANSMISSION_DOWNLOADS_PATH/incompleted"
+RUN mkdir -p "$SERVARR_APP/Homer"
+RUN mkdir p /etc/nginx/
 
 COPY nginx/** /etc/nginx/
 COPY transmission/** /etc/transmission-daemon/
 COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
 COPY install.sh /install.sh
+
 RUN chmod +x /install.sh
 RUN bash /install.sh
 
-VOLUME [ "/.config/", "/etc/nginx" ]
+COPY assets/** $SERVARR_APP/Homer/assets
+COPY assets/servarr.png $SERVARR_APP/Homer/assets/icons/favicon.ico
+
+VOLUME "$HOME/.config/"
+VOLUME "/etc/nginx" 
 VOLUME $SERVARR_APP
 VOLUME $TRANSMISSION_DOWNLOADS_PATH
 VOLUME $LOGS_PATH
--- install.sh
+++ install.sh
@@ -5,143 +5,102 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-function Config() {
-    echo "Update syteme ..."
-    apt-get -qq update 
-    echo "Install tools ..."
-    apt-get install --no-install-recommends -y -qq curl nano wget nginx sqlite3 mediainfo libchromaprint-tools \
-    nginx-extras supervisor procps ca-certificates transmission-daemon unzip
+function __set_app() {
+    app=$1
+    app_lower=$(echo "$app" | tr "[:upper:]" "[:lower:]")
+    echo "Autorisation $app in $SERVARR_APP/$app"
+    chown "$app_lower":"$app_lower" -R "$SERVARR_APP/$app"
+    "$SERVARR_APP/$app/$app" -nobrowser &
+    sleep 5s
+    sed -i "s|<UrlBase></UrlBase>|<UrlBase>/$app_lower</UrlBase>|g" "$HOME/.config/$app/config.xml"
+    sed -i "s|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g" "$HOME/.config/$app/config.xml"
+    sed -i "s|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g" "$HOME/.config/$app/config.xml"
+    pkill -f "$SERVARR_APP/$app"
+}
 
-    echo "Clean apt/lists ..."
+
+function __get_app(){
+    app=$1
+    url=$2
+    extra=$3
+    typefile=$4
+    app_lower=$(echo "$app" | tr "[:upper:]" "[:lower:]")
+
+    echo "[GET] => $app "
+    wget -q --show-progress --no-check-certificate "$extra" "$url"
+    if [[ "$typefile" == "zipfile" ]]; then
+        echo "Extract zip file $app_lower.zip in $SERVARR_APP/$app"
+        unzip -qqo "$app_lower".zip -d "$SERVARR_APP/$app"
+        echo "Delete homer.zip"
+        rm "$app_lower".zip
+    else
+        echo "Extract $app*.tar.gz"
+        tar -xvzf "$app"*.tar.gz
+        echo "Delete $app*.tar.gz"
+        rm "$app"*.tar.gz
+        echo "Move $app $SERVARR_APP/"
+        mv "$app/" "$SERVARR_APP/"
+    fi
+}
+
+function Config() {
+    echo "Update syteme..."
+    apt-get -qq update
+    echo "Install tools curl nano wget nginx sqlite3 mediainfo libchromaprint-tools nginx-extras supervisor procps ca-certificates transmission-daemon unzip"
+    apt-get install --no-install-recommends -y -qq curl nano wget nginx sqlite3 mediainfo libchromaprint-tools \
+    nginx-extras supervisor procps ca-certificates transmission-daemon unzip 
+    echo "Clean apt/lists..."
     rm -rf /var/lib/apt/lists/*
     apt-get -qq clean
     apt-get -qq autoremove -y
-
     echo "Create workspace $TRANSMISSION_DOWNLOADS_PATH"
     mkdir -p "$TRANSMISSION_DOWNLOADS_PATH/completed"
     mkdir -p "$TRANSMISSION_DOWNLOADS_PATH/incompleted"
     echo "Create Workspace $SERVARR_APP"
     mkdir -p "$SERVARR_APP"
-    return 
+    return
 }
 
 function Homer() {
-    echo "Download Homer ..."
-    wget -q --show-progress --no-check-certificate "https://github.com/bastienwirtz/homer/releases/latest/download/homer.zip" -O Homer.zip
-    echo "Unzip Homer.zip in $SERVARR_APP/Homer"
-    unzip -qq Homer.zip -d $SERVARR_APP/Homer
-    echo "Delete homer.zip"
-    rm Homer.zip
+    __get_app "Homer" "https://github.com/bastienwirtz/homer/releases/latest/download/homer.zip" --content-disposition "zipfile"
     echo "Copie assets Homer"
-    cp ./assets/** $SERVARR_APP/Homer/assets
+    cp ./assets/** "$SERVARR_APP/Homer/assets"
     echo "Edit favicon Homer"
-    cp ./assets/logo.png $SERVARR_APP/Homer/assets/icons/favicon.ico
-    return 
+    cp ./assets/logo.png "$SERVARR_APP/Homer/assets/icons/favicon.ico"
+    return
 }
 
-function Readar() {
-    echo "Download Readarr..."
-    wget -q --show-progress --no-check-certificate --content-disposition 'http://readarr.servarr.com/v1/update/develop/updatefile?os=linux&runtime=netcore&arch=x64'
-    echo "Extract Readarr"
-    tar -xvzf Readarr*.linux*.tar.gz >/dev/null 2>&1
-    echo "Move Readarr to $SERVARR_APP/"
-    mv Readarr $SERVARR_APP/
-    echo "Autorisation readarr in $SERVARR_APP/Readarr"
-    chown readarr:readarr -R $SERVARR_APP/Readarr
-    echo "Remove Readarr*.linux*.tar.gz"
-    rm Readarr*.linux*.tar.gz
-    $SERVARR_APP/Readarr/Readarr -nobrowser &
-    sleep 5s
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/readarr</UrlBase>|g' ~/.config/Readarr/config.xml
-    sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g' ~/.config/Readarr/config.xml
-    sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g' ~/.config/Readarr/config.xml
-    pkill -f $SERVARR_APP/Readarr
+function Readar() { 
+    __get_app "Readar" 'http://readarr.servarr.com/v1/update/develop/updatefile?os=linux&runtime=netcore&arch=x64' --content-disposition
+    __set_app "Readar"
     return
 }
 
 function Radarr() {
-    echo "Download Radarr..."
-    wget -q --show-progress --no-check-certificate --content-disposition 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
-    echo "Extract Radarr"
-    tar -xvzf Radarr*.linux*.tar.gz >/dev/null 2>&1
-    echo "Move Radarr $SERVARR_APP/"
-    mv Radarr $SERVARR_APP/
-    echo "Autorisation Radarr in $SERVARR_APP/Radarr"
-    chown radarr:radarr -R $SERVARR_APP/Radarr
-    echo "Remove Radarr*.linux*.tar.gz"
-    rm Radarr*.linux*.tar.gz
-    $SERVARR_APP/Radarr/Radarr -nobrowser &
-    sleep 5s
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/radarr</UrlBase>|g' ~/.config/Radarr/config.xml
-    sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g' ~/.config/Radarr/config.xml
-    sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g' ~/.config/Radarr/config.xml
-    pkill -f $SERVARR_APP/Radarr
+    __get_app "Radarr" 'http://radarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64' --content-disposition
+    __set_app "Radarr"
     return
 }
 
 function Sonarr() {
-    echo "Download Sonarr..."
-    wget -q --show-progress --no-check-certificate --content-disposition 'http://services.sonarr.tv/v1/download/master/latest?version=4&os=linux&runtime=netcore&arch=x64'
-    echo "Extract Sonarr"
-    tar -xvzf Sonarr*.linux*.tar.gz >/dev/null 2>&1
-    echo "Move Sonarr $SERVARR_APP/"
-    mv Sonarr/ $SERVARR_APP
-    echo "Autorisation Sonarr in $SERVARR_APP/Sonarr"
-    chown -R root:media $SERVARR_APP/Sonarr
-    echo "Remove Sonarr*.linux*.tar.gz"
-    rm Sonarr*.linux*.tar.gz
-    $SERVARR_APP/Sonarr/Sonarr -nobrowser &
-    sleep 5s
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/sonarr</UrlBase>|g' ~/.config/Sonarr/config.xml
-    sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g' ~/.config/Sonarr/config.xml
-    sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g' ~/.config/Sonarr/config.xml
-    pkill -f $SERVARR_APP/Sonarr
+    __get_app "Sonarr" 'http://services.sonarr.tv/v1/download/master/latest?version=4&os=linux&runtime=netcore&arch=x64' --content-disposition
+    __set_app "Sonarr"
     return
 }
 
 function Lidarr() {
-    echo "Download Lidarr..."
-    wget -q --show-progress --no-check-certificate --content-disposition 'http://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
-    echo "Extract Lidarr"
-    tar -xvzf Lidarr*.linux*.tar.gz >/dev/null 2>&1
-    echo "Move Lidarr $SERVARR_APP/"
-    mv Lidarr/ $SERVARR_APP
-    echo "Autorisation Lidarr in $SERVARR_APP/Lidarr"
-    chown -R root:media $SERVARR_APP/Lidarr
-    echo "Remove Lidarr*.linux*.tar.gz"
-    rm Lidarr*.linux*.tar.gz
-    $SERVARR_APP/Lidarr/Lidarr -nobrowser &
-    sleep 5s
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/lidarr</UrlBase>|g' ~/.config/Lidarr/config.xml
-    sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g' ~/.config/Lidarr/config.xml
-    sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g' ~/.config/Lidarr/config.xml
-    pkill -f $SERVARR_APP/Lidarr
+    __get_app "Lidarr" 'http://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64' --content-disposition
+    __set_app "Lidarr"
     return
 }
 
 function Prowlarr() {
-    echo "Download Prowlarr..."
-    wget -q --show-progress --no-check-certificate --content-disposition 'http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64'
-    echo "Extract Prowlarr"
-    tar -xvzf Prowlarr*.linux*.tar.gz >/dev/null 2>&1
-    echo "Move Prowlarr $SERVARR_APP/"
-    mv Prowlarr/ $SERVARR_APP
-    echo "Autorisation Prowlarr in $SERVARR_APP/Prowlarr"
-    chown prowlarr:prowlarr -R $SERVARR_APP/Prowlarr
-    echo "Remove Prowlarr*.linux*.tar.gz"
-    rm Prowlarr*.linux*.tar.gz
-    $SERVARR_APP/Prowlarr/Prowlarr -nobrowser &
-    sleep 5s
-    sed -i 's|<UrlBase></UrlBase>|<UrlBase>/prowlarr</UrlBase>|g' ~/.config/Prowlarr/config.xml
-    sed -i 's|<AuthenticationMethod></AuthenticationMethod>|<AuthenticationMethod>Basic</AuthenticationMethod>|g' ~/.config/Prowlarr/config.xml
-    sed -i 's|<AuthenticationRequired></AuthenticationRequired>|<AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>|g' ~/.config/Prowlarr/config.xml
-    pkill -f $SERVARR_APP/Prowlarr
+    __get_app "Prowlarr" 'http://prowlarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64' --content-disposition
+    __set_app "Prowlarr"
     return
 }
 
-Config &
-wait
-
+Config
 # Run in background for best performance
 Prowlarr &
 Readar &
@@ -158,7 +117,6 @@ sed -i "s|_SERVARR_THEME_|$SERVARR_THEME|g" /etc/nginx/theme-park.conf
 echo "Edit conf transmission"
 sed -i "s|_TRANSMISSION_DOWNLOADS_PATH_COMPLETED_|$TRANSMISSION_DOWNLOADS_PATH/completed|g" /etc/transmission-daemon/settings.json
 sed -i "s|_TRANSMISSION_DOWNLOADS_PATH_INCOMPLETED_|$TRANSMISSION_DOWNLOADS_PATH/incompleted|g" /etc/transmission-daemon/settings.json
-
 
 echo "Script Ended"
 exit 0
