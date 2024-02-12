7397a79918417dba5f6ec480fb5d83c71993de0a
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-02-12 20:18:42 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,6 +45,7 @@ echo "--> Create $SERVARR_LOG_DIR/nginx"
 mkdir -p $SERVARR_LOG_DIR/nginx
 
 echo "--> Update Nginx conf"
+wget -q https://raw.githubusercontent.com/kalibrado/servarr-dashboard/main/nginx/init-nginx.conf --no-check-certificate --content-disposition
 envsubst '$SERVARR_THEME $SERVARR_APP_DIR $SERVARR_LOG_DIR' < /etc/nginx/init-nginx.conf > /etc/nginx/nginx.conf
 
 echo "--> Create $SERVARR_CONF_DIR/transmission"
@@ -52,11 +53,12 @@ mkdir -p $SERVARR_CONF_DIR/transmission
 
 echo "--> Setup settings transmission"
 path_file="$SERVARR_CONF_DIR/transmission"
-envsubst '$TRANSMISSION_COMPLETED_DIR $TRANSMISSION_INCOMPLETED_DIR $RPC_USERNAME $RPC_AUTH_REQUIRED $RPC_PASSWORD' < "$path_file/init-settings.json" > "$path_file/settings.json"
+wget -q https://raw.githubusercontent.com/kalibrado/servarr-dashboard/main/transmission/init-settings.json --no-check-certificate --content-disposition
+envsubst '$TRANSMISSION_COMPLETED_DIR $TRANSMISSION_INCOMPLETED_DIR $RPC_USERNAME $RPC_AUTH_REQUIRED $RPC_PASSWORD' < "init-settings.json" > "$path_file/settings.json"
 
 # Create log dirs and files
-mkdir -p $( dirname $(cat /etc/supervisor/conf.d/  | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
-touch $( cat /etc/supervisor/conf.d/  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
+mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisor.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
+touch $( cat  /etc/supervisor/conf.d/supervisor.conf   | grep logfile= | grep "\.log" | sed s/.*logfile=// )
 
 # Then run supervisord
 /usr/bin/supervisord
