7eea0543879d198d8deb73756b501a0a2c2d1bd8
BlueDragon <51781584+kalibrado@users.noreply.github.com>
2024-02-12 19:34:22 +0000

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+WORKDIR=${WORKDIR:="/srv/servarr-dashboard"}
+SERVARR_APP_DIR=${SERVARR_APP_DIR:="$WORKDIR/app"}
+SERVARR_CONF_DIR=${SERVARR_CONF_DIR:="$WORKDIR/config"}
+SERVARR_LOG_DIR=${SERVARR_LOG_DIR:="$WORKDIR/log"}
 cat << EOF
 #-------------------------------------------------------------------------------------------------------------------------#
 #   _____   ___  ____   __ __   ____  ____   ____          ___     ____   _____ __ __  ____    ___    ____  ____   ___    #
@@ -19,11 +23,35 @@ cat << EOF
 #-------------------------------------------------------------------------------------------------------------------------#
 EOF
 
+echo "--> Create $SERVARR_APP_DIR"
+mkdir -p $SERVARR_APP_DIR
+echo "--> Create $SERVARR_CONF_DIR"
+mkdir -p $SERVARR_CONF_DIR
+echo "--> Create $SERVARR_LOG_DIR"
+mkdir -p $SERVARR_LOG_DIR
+echo "--> Create $SERVARR_LOG_DIR/prowlarr"
+mkdir -p $SERVARR_LOG_DIR/prowlarr 
+echo "--> Create $SERVARR_LOG_DIR/radarr"
+mkdir -p $SERVARR_LOG_DIR/radarr 
+echo "--> Create $SERVARR_LOG_DIR/sonnar"
+mkdir -p $SERVARR_LOG_DIR/sonnar
+echo "--> Create $SERVARR_LOG_DIR/lidarr"
+mkdir -p $SERVARR_LOG_DIR/lidarr
+echo "--> Create $SERVARR_LOG_DIR/readarr"
+mkdir -p $SERVARR_LOG_DIR/readarr
+echo "--> Create $SERVARR_LOG_DIR/transmission"
+mkdir -p $SERVARR_LOG_DIR/transmission
+echo "--> Create $SERVARR_LOG_DIR/nginx"
+mkdir -p $SERVARR_LOG_DIR/nginx
+
 echo "--> Update Nginx conf"
 envsubst '$SERVARR_THEME $SERVARR_APP_DIR $SERVARR_LOG_DIR' < /etc/nginx/init-nginx.conf > /etc/nginx/nginx.conf
 
+echo "--> Create $SERVARR_CONF_DIR/Transmission"
+mkdir -p $SERVARR_CONF_DIR/Transmission
+
 echo "--> Setup settings Transmission"
-path_file="$SERVARR_CONF_DIR/Transmission/"
+path_file="$SERVARR_CONF_DIR/Transmission"
 envsubst '$TRANSMISSION_COMPLETED_DIR $TRANSMISSION_INCOMPLETED_DIR $RPC_USERNAME $RPC_AUTH_REQUIRED $RPC_PASSWORD' < "$path_file/init-settings.json" > "$path_file/settings.json"
 
 # Create log dirs and files
